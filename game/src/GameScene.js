47a033f4dc458236bac267d57eb9eca1288d01b7
zhleonxu <zhleonxu@gmail.com>
2015-12-09 19:03:06 +0800

    place food and movement done

---
+++ game/src/GameScene.js
@@ -0,0 +1,98 @@
+var GameLayer = cc.Layer.extend({
+
+    ctor: function(){
+        this._super();
+        //cc.log("Game init");
+        var size = cc.director.getWinSize();
+
+        var bg = new cc.Sprite(res.game_bg_png);
+        bg.x = size.width/2;
+        bg.y = size.height/2;
+        this.addChild(bg,0);
+
+        var MAX_FOOD_NUM = 100;
+        var food = new Array(MAX_FOOD_NUM);
+        var food_index = 0;
+
+        /* real code when server is online
+        io.emit('place_food',function(food_pos_x, food_pos_y){
+            var random_num = Math.round(Math.random()*3);
+            if(random_num == 0) food[food_index] = new cc.Sprite(res.food_red_png);
+            if(random_num == 1) food[food_index] = new cc.Sprite(res.food_blue_png);
+            if(random_num == 2) food[food_index] = new cc.Sprite(res.food_green_png);
+            if(random_num == 3) food[food_index] = new cc.Sprite(res.food_purple_png);
+            food[food_index].setAnchorPoint(0.5, 0.5);
+            food[food_index].setPosition(food_pos_x, food_pos_y);
+            this.addChild(food[food_index],0);
+            food_index++;
+        });
+        */
+
+
+        //The following is for demo
+        for(var i=0;i<50;i++){
+            var food_pos_x = Math.round(Math.random()*size.width);
+            var food_pos_y = Math.round(Math.random()*size.height);
+
+            var random_num = Math.round(Math.random()*3);
+            if(random_num == 0) food[food_index] = new cc.Sprite(res.food_red_png);
+            if(random_num == 1) food[food_index] = new cc.Sprite(res.food_blue_png);
+            if(random_num == 2) food[food_index] = new cc.Sprite(res.food_green_png);
+            if(random_num == 3) food[food_index] = new cc.Sprite(res.food_purple_png);
+            food[food_index].setAnchorPoint(0.5, 0.5);
+            food[food_index].setPosition(food_pos_x, food_pos_y);
+            this.addChild(food[food_index],0);
+            food_index++;
+        }
+
+        // demo ended
+
+
+        var ball = new cc.Sprite(res.ball_png);
+        ball.setAnchorPoint(0.5, 0.5);
+        ball.setPosition(size.width/2, size.height/2);
+        this.addChild(ball,0);
+
+        var REFRESH_RATE = 10;
+        var speed = 1;
+        var mousePos;
+        cc.eventManager.addListener({
+            event: cc.EventListener.MOUSE,
+            onMouseMove: function (event) {
+                mousePos = event.getLocation();
+            }
+        },ball);
+
+        window.setInterval(function(){
+                    var diff_x = mousePos.x - ball.x;
+                    var diff_y = mousePos.y - ball.y;
+                    var distance = Math.sqrt((diff_x * diff_x) + (diff_y * diff_y));
+                    var sin = diff_y/distance;
+                    var cos = diff_x/distance;
+                    ball.x = ball.x + speed*cos;
+                    ball.y = ball.y + speed*sin;
+        }, REFRESH_RATE);
+
+
+
+
+
+
+
+        return true;
+
+
+    }
+
+
+});
+
+var GameScene = cc.Scene.extend({
+    onEnter:function(){
+        this._super();
+        var layer = new GameLayer;
+        this.addChild(layer);
+        layer.init();
+    }
+});
+
---
+++ game/src/resource.js
@@ -0,0 +1,21 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+    MainScene_json : "res/MainScene.json",
+    start_up_png : "res/start_up.png",
+    start_down_png : "res/start_down.png",
+    about_up_png : "res/about_up.png",
+    about_down_png : "res/about_down.png",
+    first_bg_png : "res/first_bg.png",
+    game_bg_png : "res/game_bg.png",
+    ball_png : "res/ball.png",
+    enemy_png : "res/enemy.png",
+    food_red_png : "res/food_red.png",
+    food_blue_png : "res/food_blue.png",
+    food_green_png : "res/food_green.png",
+    food_purple_png : "res/food_purple.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
